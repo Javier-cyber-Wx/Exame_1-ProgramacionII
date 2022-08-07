9b738c95998d3293cb383147e83b8fca7b2b0267
Javier Chamo <francisco12102002cl@gmail.com>
2022-08-07 14:03:52 -0600

    Se coloco variables que faltaban

--- src/main/java/Paquete_examen/ShippingInfo.java
+++ src/main/java/Paquete_examen/ShippingInfo.java
@@ -5,14 +5,19 @@ public class ShippingInfo {
     private int shippingID;
     private String shippingType;
     private int shippingRegionID;
+    private int shoppingCost;
 
     private Order_1 order;
-    
-    public ShippingInfo(int shippingID, String shippingType, int shippingRegionID) {
+
+    public ShippingInfo(int shippingID, String shippingType, int shippingRegionID, int shoppingCost, Order_1 order) {
         this.shippingID = shippingID;
         this.shippingType = shippingType;
         this.shippingRegionID = shippingRegionID;
+        this.shoppingCost = shoppingCost;
+        this.order = order;
     }
+    
+    
     
     public void updateShippingInfo()
     {
